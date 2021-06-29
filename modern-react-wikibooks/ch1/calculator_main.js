fad4fcdb26c474c336fe0b9afdc7733691254a8c
안동수 <dongsu.an@navercorp.com>
2021-06-29 23:06:15 +0900

    모듈 실습

---
+++ modern-react-wikibooks/ch1/calculator_main.js
@@ -0,0 +1,6 @@
+// read module
+const calculator = require('./calculator_module.js')
+
+// use module function
+console.log('3+5 = ', calculator.add(3,5))
+console.log('4*8 = ', calculator.add(4,8))
---
+++ modern-react-wikibooks/ch1/calculator_module.js
@@ -0,0 +1,13 @@
+// 덧셈과 곱셈하는 함수
+function add (a, b) {
+    return a + b
+}
+
+function mul (a, b) {
+    return a * b
+}
+
+module.exports = {
+    'add' : add,
+    'mul' : mul
+}
