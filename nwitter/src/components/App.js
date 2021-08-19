836311716c424cf2e4cd533a9b4d4629337a0632
dsahn <ads50112@gmail.com>
2021-08-20 02:42:40 +0900

    login, error 메시지 출력 기능 추가

--- nwitter/src/components/App.js
+++ nwitter/src/components/App.js
@@ -1,13 +1,26 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import AppRouter from "components/Router";
 import { authService } from "fbase";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
+  const [init, setInit] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  // currentUser 에서는 아직 firebase가 구동되지 않아, 로그인 여부를 모름
+  // 따라서 구동후 로그인 여부를 확인할 수 있는 onAuthStateChanged 를 사용한다
+  useEffect(()=> {
+    authService.onAuthStateChanged((user) => {
+      if (user) {
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+    })
+  }, [])
 
   return (
     <>
-    <AppRouter isLoggedIn={isLoggedIn}/>
+    {init ? <AppRouter isLoggedIn={isLoggedIn} /> : <h1>Initializing...</h1>}
     <footer>&copy; Nwitter { new Date().getFullYear() }</footer>
     </>
   );
