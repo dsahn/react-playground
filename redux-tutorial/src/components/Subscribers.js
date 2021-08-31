bd6300ff722f25fb7f6665737c37115f2582d4c3
dsahn <ads50112@gmail.com>
2021-09-01 01:38:25 +0900

    Views 로직 추가

--- redux-tutorial/src/components/Subscribers.js
+++ redux-tutorial/src/components/Subscribers.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { connect } from 'react-redux';
-import { addSubscriber } from '../redux/subscribers/actions';
+import { addSubscriber } from '../redux/index';
 
 const Subscribers = ({ count, addSubscriber }) => {
     return (
---
+++ redux-tutorial/src/components/Views.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { connect } from 'react-redux';
+import { addView } from '../redux';
+
+const Views = ({ count, addView }) => {
+    return (
+        <div className="items">
+            <h2>조회 수 : {count}</h2>
+            <button onClick={() => addView()}>조회하기!</button>
+        </div>
+    )
+}
+
+const mapStateToProps = ({ views }) => {
+    return {
+        count: views.count
+    }
+}
+
+const mapDispatchToProps = {
+    addView
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Views)
