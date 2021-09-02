b39aa3dea227eda6c797f554e7ba78bf0e788808
dsahn <ads50112@gmail.com>
2021-09-03 01:32:33 +0900

    logger 추가

--- redux-tutorial/src/redux/store.js
+++ redux-tutorial/src/redux/store.js
@@ -1,6 +1,10 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from './rootReducer';
+import logger from 'redux-logger';
 
-const store = createStore(rootReducer);
+const middleware = [logger]
+
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(...middleware)));
 
 export default store;
