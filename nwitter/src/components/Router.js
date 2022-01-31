b2e752068832d232a0fc9514ebc68402403630d2
dongsu <dongsu.an@navercorp.com>
2022-01-31 20:19:35 +0900

    react-router-dom v5->v6

--- nwitter/src/components/Router.js
+++ nwitter/src/components/Router.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { HashRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
-import Auth from '../routes/Auth';
-import Home from '../routes/Home';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import Auth from 'routes/Auth';
+import Home from 'routes/Home';
 import Navigation from 'components/Navigation';
 import Profile from 'routes/Profile';
 
@@ -9,24 +9,22 @@ const AppRouter = ({ isLoggedIn, userObj }) => {
     return (
         <Router>
             {isLoggedIn && <Navigation />}
-            <Switch>
+            <Routes>
                 {isLoggedIn ? (
                     <>
-                        <Route exact path="/">
-                            <Home userObj={userObj} />
+                        <Route path="/" element={<Home userObj={userObj} />}>
                         </Route>
-                        <Route exact path="/profile">
-                            <Profile />
+                        <Route path="/profile" element={<Profile />}>
                         </Route>
-                        <Redirect from="*" to="/" />
+                        <Route from="*" to="/" />
                     </>
                 ) : (
                     <>
-                        <Route><Auth /></Route>
-                        <Redirect from="*" to="/" />
+                        <Route path="/" element={<Auth />}></Route>
+                        <Route from="*" to="/" />
                     </>
                 )}
-            </Switch>
+            </Routes>
         </Router>
     )
 }
