c5fec61121af24b3c6a07be40b4ef43f432e751f
dsahn <ads50112@gmail.com>
2022-02-03 22:45:11 +0900

    이미지 업로드 기능 추가

--- nwitter/src/routes/Home.js
+++ nwitter/src/routes/Home.js
@@ -5,6 +5,7 @@ import Nweet from 'components/Nweet';
 const Home = ({ userObj }) => {
     const [nweet, setNweet] = useState("")
     const [nweets, setNweets] = useState([])
+    const [attachment, setAttachment] = useState()
 
     // snapshot 을 사용하면 re-render 하지 않아도 실시간 출력된다.
     useEffect(() => {
@@ -32,6 +33,17 @@ const Home = ({ userObj }) => {
         const { target: { value } } = event;
         setNweet(value);
     }
+    const onFileChange = (event) => {
+        const { target: { files } } = event;
+        const theFile = files[0];
+        const reader = new FileReader();
+        reader.onloadend = (finishedEvent) => {
+            const { currentTarget: { result } } = finishedEvent
+            setAttachment(result)
+        }
+        reader.readAsDataURL(theFile)
+    }
+    const onClearAttachment = () => setAttachment(null)
     return (
         <div>
             <form onSubmit={onSubmit}>
@@ -40,7 +52,12 @@ const Home = ({ userObj }) => {
                     type="text"
                     placeholder="What's on your mind?"
                     maxLength={120} />
+                <input type="file" accept='image/*' onChange={onFileChange} />
                 <input type="submit" value="Nweet" />
+                {attachment && <div>
+                    <img src={attachment} width="50px" height="50px" alt="thumbnail" />
+                    <button onClick={onClearAttachment}>Clear</button>
+                </div>}
             </form>
             <div>
                 {nweets.map((nweet) => (
